e6d272da883eb469652501dc56a1224ee503939a
Goc-ong, Reymart V. <sinvictus20@gmail.com>
2026-06-06 01:08:01 +0800

    Commit

---
+++ new-game-project/jeepney.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const GRAVITY : int = 4200
+const JUMP_SPEED : int = -1800
+
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_SPEED
+		else:
+			$AnimatedSprite2D.play("running")
+	else:
+		$AnimatedSprite2D.play("jump")
+
+	move_and_slide()
